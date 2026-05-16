ed4a43c416525969014a5d01ed0480e7f4b544e9
Waleed Jawad <240048282@aston.ac.uk>
2026-05-16 08:37:57 +0100

    chore: initial Spring Boot project setup

---
+++ src/test/java/com/studyhub/backend/StudyhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
